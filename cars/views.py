4db76d7710107030ac3ce4614c4b82ba26ddb66d
Boburbek <boburbekinomov684@gmail.com>
2025-05-28 15:20:01 +0500

    sss

--- cars/views.py
+++ cars/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from cars.models import Car, Brand
 
@@ -11,21 +11,28 @@ def base(request):
 
 
 # def home(request):
+from .models import Car, Brand
+
 def filter(request):
     if request.method == 'POST':
-        brand_id = request.POST.get('brand')
-        price_range = request.POST.get('price')
+        brand = request.POST.get('brand', 'all')
+        price = request.POST.get('price', 'all')
+
         cars = Car.objects.all()
-        brands=Brand.objects.all()
 
-        if brand_id != 'all':
-            cars = cars.filter(brand_id=brand_id)
+        if brand != 'all':
+            cars = cars.filter(brand_id=brand)
 
-        if price_range == 'low':
-            cars = cars.filter(price__lte=20000)
-        elif price_range == 'medium':
-            cars = cars.filter(price__gt=20000, price__lte=50000)
-        elif price_range == 'high':
-            cars = cars.filter(price__gt=50000)
+        if price != 'all':
+            if price == 'low':
+                cars = cars.filter(price__lte=20000)
+            elif price == 'medium':
+                cars = cars.filter(price__gt=20000, price__lte=50000)
+            elif price == 'high':
+                cars = cars.filter(price__gt=50000)
 
-        return render(request, 'index.html', {'cars': cars,'brands': brands})
+        brands = Brand.objects.all()
+        return render(request, 'index.html', {'cars': cars, 'brands': brands})
+
+    # Fallback for GET
+    return redirect('your_default_view')
